4bdfe83a17095cd2066815e7b6a3a28c66de6a17
Anton Galich <inthesky133@gmail.com>
2022-01-31 14:26:49 +0300

    fix

--- src/http/AuthApi.ts
+++ src/http/AuthApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const AUTH_API_URL = `http://localhost:5656/auth`;
+export const AUTH_API_URL = `https://my-blog-diplom.herokuapp.com/auth`;
 
 const $authApi = axios.create({
   baseURL: AUTH_API_URL,
