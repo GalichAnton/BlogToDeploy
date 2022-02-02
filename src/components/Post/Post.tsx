cd099383c2730f3d2f0ba1e44325e99dca7c7891
Anton Galich <inthesky133@gmail.com>
2022-02-02 12:15:45 +0300

    fix

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -38,7 +38,7 @@ const Post: FC<IProps> = ({ post }) => {
             src={
               post.photoUrl.startsWith('http')
                 ? post.photoUrl
-                : `${process.env.REACT_APP_CONTENT_API_URL}${post.photoUrl}`
+                : `${post.photoUrl}`
             }
             alt="test"
           />
